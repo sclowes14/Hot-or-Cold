5186217e5822ba8ecb0fe79679d8420e1d03c497
Stephen Clowes <sclowes14@gmail.com>
2015-12-29 18:49:56 -0500

    Update id names

--- script.js
+++ script.js
@@ -15,23 +15,30 @@ var previousGuesses = [];
 var correctGuess = false;
 
 // Prints an array (previousGuesses) as an ordered list 
-function printList( list ) {
+function printGuesses( guesses ) {
 	var listHTML = "<ol>";
 	// A for loop to index the array and create list items for each value
-	for ( var i = 0; i < list.length; i += 1 ) {
-		listHTML += "<li>" + list[i] + "</li>";
+	for ( var i = 0; i < guesses.length; i += 1 ) {
+		listHTML += "<li>" + guesses[i] + "</li>";
 	}
 	listHTML += "</ol>";
 	document.write( listHTML );
 }
 
-while ( guess !== answer ) {
-  // Change the color and text to warmer or colder
-  // Add guess to previousGuesses and write to the doc
-  // Add to the # of attempts
-  attempts += 1; 
-  // Add guess to previousGuesses array
-  previousGuesses.push(guess); 
-  // Print previousGuesses to the page
-  printList(previousGuesses);
+// Store the button as a variable
+guess = document.getElementById("guess-button");
+
+guess.onclick = function () {
+	// Functions for updating the color and previousGuesses array
 }
+
+// while ( guess !== answer ) {
+//   // Change the color and text to warmer or colder
+//   // Add guess to previousGuesses and write to the doc
+//   // Add to the # of attempts
+//   attempts += 1; 
+//   // Add guess to previousGuesses array
+//   previousGuesses.push(guess); 
+//   // Print previousGuesses to the page
+//   printList(previousGuesses);
+// }
